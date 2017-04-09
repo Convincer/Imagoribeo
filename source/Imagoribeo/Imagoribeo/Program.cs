cac18fd7bb2952c0cb1b0634965ea1682014658f
Ricco Rossberg <mail@ricco-rossberg.de>
2017-04-09 19:17:29 +0200

    project init

---
+++ source/Imagoribeo/Imagoribeo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Imagoribeo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
